bd9d5f16bffb5d81b7ee854eb10b536a598d9892
NoohAlavi <59208101+NoohAlavi@users.noreply.github.com>
2020-02-26 12:25:11 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,31 @@
+window.onload = setup;
+window.onkeydown = input;
+
+function setup()
+{
+  var ctx = cvs.getContext("2d");
+  
+  loop();
+}
+
+function loop()
+{
+  update();
+  draw();
+  window.requestAnimationFrame(loop);
+}
+
+function draw()
+{
+
+}
+
+function update()
+{
+
+}
+
+function input()
+{
+
+}
