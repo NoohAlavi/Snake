1a19012a8cf7eb18e72eae707b2227fa83cefbe3
NoohAlavi <59208101+NoohAlavi@users.noreply.github.com>
2020-02-26 12:51:05 -0500

    Update main.js

--- main.js
+++ main.js
@@ -30,5 +30,17 @@ function update()
 
 function input(e)
 {
-
+  if (e.key == "ArrowRight" || e.key == "d")
+  {
+    Snake.setDir(1, 0); 
+  } else if (e.key == "ArrowLeft" || e.key == "a")
+  {
+    Snake.setDir(-1 ,0); 
+  }  else if (e.key == "ArrowUp" || e.key == "w") 
+  {
+    Snake.setDir(0, -1); 
+  } else if (e.key == "ArrowDown" || e.key == "s")
+  {
+    Snake.setDir(0, 1); 
+  }
 }
