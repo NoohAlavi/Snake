a3eb8e04369b99d3d4e15982c7d87a7877841c8c
NoohAlavi <59208101+NoohAlavi@users.noreply.github.com>
2020-02-26 12:44:46 -0500

    Update snake.js

--- snake.js
+++ snake.js
@@ -4,17 +4,19 @@ class Snake
     x: 0,
     y: 0
   };
-  static width = 20;
-  static height = 20;
+  static size = 20;
+  static xSpeed = 1;
+  static ySpeed = 0;
   
   static update()
   {
-  
+    Snake.position.x += Snake.xSpeed * Snake.size;
+    Snake.position.y += Snake.ySpeed * Snake.size;
   }
   
   static draw()
   {
     ctx.fillStyle = "#00ff00";
-    ctx.fillRect(Snake.position.x, Snake.position.y, Snake.width, Snake.height);
+    ctx.fillRect(Snake.position.x, Snake.position.y, Snake.size, Snake.size);
   }
 }
